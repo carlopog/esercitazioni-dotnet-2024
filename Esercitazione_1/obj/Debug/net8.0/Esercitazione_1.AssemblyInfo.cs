7edbecea9a53fbd95b303aee5bad142524d6ffc5
carlopog <carlo.poggi98@gmail.com>
2024-01-09 10:06:36 +0100

    program con metodo main

--- Esercitazione_1/obj/Debug/net8.0/Esercitazione_1.AssemblyInfo.cs
+++ Esercitazione_1/obj/Debug/net8.0/Esercitazione_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Esercitazione_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23aaac452a9865f550088654aab11517747af4fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bebd239dd51297877ac255fd35e42b3af20a3d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Esercitazione_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Esercitazione_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
