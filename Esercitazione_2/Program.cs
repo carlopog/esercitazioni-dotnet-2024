0b3a051acc51b686fd86abfb6edc68bfec57a3a4
carlopog <carlo.poggi98@gmail.com>
2024-01-11 09:17:13 +0100

    array

--- Esercitazione_2/Program.cs
+++ Esercitazione_2/Program.cs
@@ -2,16 +2,10 @@ class Program
 {
   static void Main(string[] args)
   {
-    int a = 9;
-    int b = 5;
-    int c = 5;
-    bool maggiore = a > b;
-    bool maggioreUguale = c >= b;
-    bool minoreUguale = c <= b;
-    bool minore = a < b;
-    Console.WriteLine($"a e' maggiore di b? {maggiore}");
-    Console.WriteLine($"a e' minore di b? {minore}");
-    Console.WriteLine($"c e' maggiore uguale di b? {maggioreUguale}");
-    Console.WriteLine($"c e' minore uguale di b? {minoreUguale}");
+    string[] nomi = new string[3];
+     nomi[0] = "Mario";
+     nomi[1] = "Luigi";
+     nomi[2] = "Giovanni";
+     Console.WriteLine($"Ciao {nomi[0]}, {nomi[1]} e {nomi[2]}");
   }
 }
--- Esercitazione_2/README.md
+++ Esercitazione_2/README.md
@@ -152,3 +152,19 @@
     }
   }
 ```
+
+### 10 - Aggiungere degli elementi nelle liste e negli array
+```c#
+  class Program10
+  {
+    static void Main(string[] args)
+    {
+     string[] nomi = new string[3];
+     nomi[0] = "Mario";
+     nomi[1] = "Luigi";
+     nomi[2] = "Giovanni";
+     Console.WriteLine($"Ciao {nomi[0]}, {nomi[1]} e {nomi[2]}");
+    }
+  }
+```
+
