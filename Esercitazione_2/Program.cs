3c079d4ace6ba058af94bc2fc18b5a79723a55fe
carlopog <carlo.poggi98@gmail.com>
2024-01-12 10:18:00 +0100

    dictionary fast

--- Esercitazione_2/Program.cs
+++ Esercitazione_2/Program.cs
@@ -2,10 +2,12 @@ class Program
 {
   static void Main(string[] args)
   {
-    Dictionary<string, string> nomi = new Dictionary<string, string>();
-    nomi.Add("Mario", "Rossi");
-    nomi.Add("Luigi", "Biancoli");
-    nomi.Add("Giuseppe", "Verdi");
+    Dictionary<string, string> nomi = new()
+    {
+        { "Mario", "Rossi" },
+        { "Luigi", "Biancoli" },
+        { "Giuseppe", "Verdi" }
+    };
     Console.WriteLine($"Ciao {nomi["Mario"]}, {nomi["Luigi"]} e {nomi["Giuseppe"]}");
   }
 }
--- Esercitazione_2/README.md
+++ Esercitazione_2/README.md
@@ -436,3 +436,23 @@
 
 ```
 
+### 23 - Dichiarare un dizionario di stringhe velocemente
+
+```c#
+
+  class Program23
+
+    {
+      static void Main(string[] args)
+      {
+        Dictionary<string, string> nomi = new()
+        {
+            { "Mario", "Rossi" },
+            { "Luigi", "Biancoli" },
+            { "Giuseppe", "Verdi" }
+        };
+        Console.WriteLine($"Ciao {nomi["Mario"]}, {nomi["Luigi"]} e {nomi["Giuseppe"]}");
+      }
+    }
+
+```
