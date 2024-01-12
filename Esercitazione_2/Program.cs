9c3bc1dbd296f2b7f0b1e537ba43d396c363240b
carlopog <carlo.poggi98@gmail.com>
2024-01-12 09:35:14 +0100

    else

--- Esercitazione_2/Program.cs
+++ Esercitazione_2/Program.cs
@@ -3,10 +3,12 @@ class Program
   static void Main(string[] args)
   {
     int a = 10;
-    int b = 2;
+    int b = 20;
     if (a > b)
     {
       Console.WriteLine($"{a} e' maggiore di {b}");
     }
+    else
+    Console.WriteLine($"{a} e' minore di {b}");
   }
 }
