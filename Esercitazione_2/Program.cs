d7429fe69f2579f386937188bd3bb871d710546b
carlopog <carlo.poggi98@gmail.com>
2024-01-15 12:50:16 +0100

    Add name cmd

--- Esercitazione_2/Program.cs
+++ Esercitazione_2/Program.cs
@@ -2,33 +2,37 @@ using System;
 
 class Example
 {
-   public static void Main()
-   {  
+  public static void Main()
+  {
+    var nome = "Giammarco";
     while (true)
     {
       string input = Console.ReadLine();
-      
+
       if (input.StartsWith("cmd:"))
       {
         string comando = input.Substring(4);
-        switch (comando.ToLower()) 
+        switch (comando.ToLower())
         {
-        case "info":
-          Console.WriteLine("Comando info riconosciuto. Mostro le informazioni");
-          break;        
-        case "exit":
-          Console.WriteLine("Comando exit riconosciuto. Uscita in corso...");
-          return; // esce dal programma
-        default: 
-          Console.WriteLine($"Comando {comando} non riconosciuto.");
-          break;
-      }
+          case "info":
+            Console.WriteLine("Comando info riconosciuto. Mostro le informazioni");
+            break;
+          case "name":
+            Console.WriteLine($"Ciao {nome}");
+            break;
+          case "exit":
+            Console.WriteLine("Comando exit riconosciuto. Uscita in corso...");
+            return; // esce dal programma
+          default:
+            Console.WriteLine($"Comando {comando} non riconosciuto.");
+            break;
+        }
       }
       else
       {
         Console.WriteLine("Input non valido. Inserisci un comando");
       }
-        Console.WriteLine("\n Inserisci un altro comando:");
+      Console.WriteLine("\n Inserisci un altro comando:");
     }
-   }
+  }
 }
