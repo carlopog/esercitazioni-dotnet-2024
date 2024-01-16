da9a3cc68ca372eb325fed84eb98971b3ccbcf6d
carlopog <carlo.poggi98@gmail.com>
2024-01-16 12:39:58 +0100

    fix programma

--- Esercitazione_2/Program.cs
+++ Esercitazione_2/Program.cs
@@ -13,43 +13,36 @@ class Program2
 
     Console.WriteLine("nella mia famiglia ci sono:");
 
-    foreach (string key in famiglia.Keys) 
+    foreach (string key in famiglia.Keys)
     {
       Console.WriteLine($"{key}");
     }
-      
-      bool uscita = true;
-      while (uscita)
-      { 
- 
+
+    while (true)
+    {
+
       Console.WriteLine("Di chi vuoi sapere l'eta?");
       string? scelta = Console.ReadLine();
       switch (scelta)
-        {
-          case "Giovanna":
-            Console.WriteLine("Non si chiede l'eta' di una signora");
-            break;
-          case "Carlo":
-            Console.WriteLine($"io ho {famiglia[scelta]} anni");
-            break;
-          case "Elvis": case "Simone": case "Maurizio":
-            Console.WriteLine($"{scelta} ha {famiglia[scelta]} anni");
-            break;
-          case "x": 
-            Console.WriteLine($"{scelta} uscita in corso");
+      {
+        case "Giovanna":
+          Console.WriteLine("Non si chiede l'eta' di una signora");
+          break;
+        case "Carlo":
+          Console.WriteLine($"io ho {famiglia[scelta]} anni");
+          break;
+        case "Elvis":
+        case "Simone":
+        case "Maurizio":
+          Console.WriteLine($"{scelta} ha {famiglia[scelta]} anni");
+          break;
+        case "x":
+          Console.WriteLine($"{scelta} uscita in corso");
           return;
-          default: 
-            Console.WriteLine($"{scelta} scelta non valida");
-          return;
-        }
+        default:
+          Console.WriteLine($"{scelta} scelta non valida");
+          break;
       }
+    }
   }
 }
-
-
-      // int i = 0;
-      // while (i < parenti.Length - 1)
-      // {
-      //   Console.WriteLine($" {parenti[i]},");
-      //   i++;
-      // }
