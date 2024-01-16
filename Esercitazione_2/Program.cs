28ad86394138c196038b6caff8b077b620885cfc
carlopog <carlo.poggi98@gmail.com>
2024-01-16 09:32:51 +0100

    programma simone

--- Esercitazione_2/Program.cs
+++ Esercitazione_2/Program.cs
@@ -1,31 +1,74 @@
 using System;
+using System.Collections;
 
 class Example
 {
   public static void Main()
   {
-    Console.Title = "La mia applicazione web";
-    Console.CursorVisible = false; 
+    Console.Clear();
+    Console.WriteLine("Seleziona l'opzione:");
+    Console.WriteLine("1 - Nascondi il cursore");
+    Console.WriteLine("2 - Mostra il cursore");
+    Console.WriteLine("3 - Pulisci console");
+    Console.WriteLine("4 - Emetti beep");
+    Console.WriteLine("5 - Emetti beep prolungato");
+    Console.WriteLine("6 - Imposta il titolo");
+    Console.WriteLine("e - Exit\n");
 
-    Console.WriteLine("come stai?");
-    string? input = Console.ReadLine();
-    if (input == "bene")
+    while (true)
     {
-      Console.WriteLine("Bravo.");
-      Console.Beep(750, 300);
+      Console.WriteLine("Digita...");
+
+      string? input = Console.ReadLine();
+      switch (input)
+      {
+        case "1":
+          Console.CursorVisible = false;
+          break;
+
+        case "2":
+          Console.CursorVisible = true;
+          break;
+
+        case "3":
+          Console.Clear();
+          break;
+
+        case "4":
+          Console.Beep();
+          break;
+
+        case "5":
+          Console.WriteLine("Inserisci la frequenza");
+          int freq = Int32.Parse(Console.ReadLine());
+          Console.WriteLine("Inserisci i millisecondi");
+          int ms = Int32.Parse(Console.ReadLine());
+          Console.Beep(freq, ms);
+          break;
+
+        case "6":
+          Console.Title = "La mia app";
+          break;
+
+        case "e":
+          return;
+
+        default:
+          Console.WriteLine("Comando non valido riprova");
+          break;
+
+      }
+
+      Console.Clear();
+      Console.WriteLine("Seleziona l'opzione:");
+      Console.WriteLine("1 - Nascondi il cursore");
+      Console.WriteLine("2 - Mostra il cursore");
+      Console.WriteLine("3 - Pulisci console");
+      Console.WriteLine("4 - Emetti beep");
+      Console.WriteLine("5 - Emetti beep prolungato");
+      Console.WriteLine("6 - Imposta il titolo");
+      Console.WriteLine("e - Exit\n");
+
     }
-    else if (input == "male")
-    {
-      Console.WriteLine("cattivo");
-      Console.Beep(800, 1000); 
-    }
-    else 
-    {
-      Console.WriteLine("ci sta");
-      Console.Beep(38, 200); 
-      Console.Beep(300, 600); 
-      Console.Beep(500, 500); 
-    }
-    // Console.Clear();
   }
 }
