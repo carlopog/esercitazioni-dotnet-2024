51f925af9c8b88c74e2b419a89ed40ebb36887b0
carlopog <carlo.poggi98@gmail.com>
2024-01-17 12:55:04 +0100

    liste fizz buzz

--- Esercitazione_2/Program.cs
+++ Esercitazione_2/Program.cs
@@ -1,30 +1,54 @@
+using System.ComponentModel;
 using Microsoft.VisualBasic;
 
 class Program2
 {
   static void Main(string[] args)
   {
-    // stampa i numeri tra 1 e 100
-    for (int i = 1; i < 101; i++ )
-    {
+      List<int> fizzibuzzi= [15];
+      List<int> fizzi= [3];
+      List<int> buzzi= [5];
+      Random random = new();
+      for (int a = 0; a < 15; a++)
       {
-        bool fizz = i % 3 == 0;
-        bool buzz = i % 5 == 0;
-        Console.Write($"{i} ");
-    
-         if (fizz)
-        {
-          Console.Write("fizz");
-        }
-         if (buzz)
-        {
-          Console.WriteLine("buzz");
-        }
-        else
-        {
-          Console.WriteLine("");
-        }
+        int i = random.Next(1, 101); // genera un numero tra 1 e 100
+        
+          bool fizz = i % 3 == 0;
+          bool buzz = i % 5 == 0;
+  
+          if (fizz && buzz)
+          {
+            fizzibuzzi.Add(i);
+          }
+          else if (fizz)
+          {
+            fizzi.Add(i);
+
+          }
+          else if (buzz)
+          {
+            buzzi.Add(i);
+          }
+          else
+          {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{i} fuori lista");
+            Console.ResetColor();
+          }
       }
-    }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"FizziBuzzi ha {fizzibuzzi.Count} elementi");
+        fizzibuzzi.ForEach(fb => Console.Write($"{fb}  "));
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"\nFizzi  ha {fizzi.Count} elementi");
+        fizzi.ForEach(f => Console.Write($"{f}  "));
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"\nBuzzi  ha {buzzi.Count} elementi");
+        buzzi.ForEach(b => Console.Write($"{b}  "));
+        Console.ResetColor();
   }
 }
--- Esercitazione_2/README.md
+++ Esercitazione_2/README.md
@@ -1206,11 +1206,11 @@ class Program50
       for (int a = 0; a < 5; a++)
       {
         int i = random.Next(1, 101); // genera un numero tra 1 e 100
-  
+
         {
           bool fizz = i % 3 == 0;
           bool buzz = i % 5 == 0;
-  
+
           if (fizz && buzz)
           {
             Console.WriteLine($"{i} fizzbuzz");
@@ -1264,4 +1264,54 @@ class Program51
       }
     }
   }
+```
+
+### 52 - Esercizio stampa liste fizzi e buzzi colorate diversamente
+
+```c#
+      List<int> fizzibuzzi= [15];
+      List<int> fizzi= [3];
+      List<int> buzzi= [5];
+      Random random = new();
+      for (int a = 0; a < 15; a++)
+      {
+        int i = random.Next(1, 101); // genera un numero tra 1 e 100
+        
+          bool fizz = i % 3 == 0;
+          bool buzz = i % 5 == 0;
+  
+          if (fizz && buzz)
+          {
+            fizzibuzzi.Add(i);
+          }
+          else if (fizz)
+          {
+            fizzi.Add(i);
+
+          }
+          else if (buzz)
+          {
+            buzzi.Add(i);
+          }
+          else
+          {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{i} fuori lista");
+            Console.ResetColor();
+          }
+      }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"FizziBuzzi ha {fizzibuzzi.Count} elementi");
+        fizzibuzzi.ForEach(fb => Console.Write($"{fb}  "));
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"\nFizzi  ha {fizzi.Count} elementi");
+        fizzi.ForEach(f => Console.Write($"{f}  "));
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"\nBuzzi  ha {buzzi.Count} elementi");
+        buzzi.ForEach(b => Console.Write($"{b}  "));
+        Console.ResetColor();
 ```
