ecb850b73b5ed8d28727a20106cdde78ae7ec8d9
carlopog <carlo.poggi98@gmail.com>
2024-01-19 15:31:41 +0100

    guess the number fiz

--- Esercitazione_2/Program.cs
+++ Esercitazione_2/Program.cs
@@ -1,60 +1,49 @@
-using System.ComponentModel;
-using Microsoft.VisualBasic;
 
-class Program2
+    class Program
 {
-  static void Main(string[] args)
-  {
-      List<int> fizzibuzzi= [15];
-      List<int> fizzi= [3];
-      List<int> buzzi= [5];
-      Random random = new();
-      for (int a = 0; a < 15; a++)
-      {
-        int i = random.Next(1, 101); // genera un numero tra 1 e 100
-        
-          bool fizz = i % 3 == 0;
-          bool buzz = i % 5 == 0;
-  
-          if (fizz && buzz)
-          {
-            fizzibuzzi.Add(i);
-            //fizzibuzzi.Add.ToString()
-          }
-          else if (fizz)
-          {
-            fizzi.Add(i);
-          }
-          else if (buzz)
-          {
-            buzzi.Add(i);
-          }
-          else
-          {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{i} fuori lista");
-            Console.ResetColor();
-          }
-      }
-        Console.ForegroundColor = ConsoleColor.Green;
-        fizzibuzzi = fizzibuzzi.Distinct().ToList();
-        fizzibuzzi.Sort(); // per ordinarli 
-        Console.WriteLine($"FizziBuzzi ha {fizzibuzzi.Count} elementi");
-        fizzibuzzi.ForEach(fb => Console.Write($"{fb}  "));
-        Console.ResetColor();
-
-        Console.ForegroundColor = ConsoleColor.Blue;
-        fizzi = fizzi.Distinct().ToList();
-        fizzi.Sort();
-        Console.WriteLine($"\nFizzi  ha {fizzi.Count} elementi");
-        fizzi.ForEach(f => Console.Write($"{f}  "));
-        Console.ResetColor();
-
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        buzzi = buzzi.Distinct().ToList();
-        buzzi.Sort();
-        Console.WriteLine($"\nBuzzi  ha {buzzi.Count} elementi");
-        buzzi.ForEach(b => Console.Write($"{b}  "));
-        Console.ResetColor();
-  }
+    static void Main(string[] args)
+    {
+        int punteggio = 0;
+        while (true)
+        {
+            Random random = new Random();
+            int numero = random.Next(1, 11);
+            Console.WriteLine("Indovina il numero sorteggiato");
+            int tentativi = 0;
+            while (true)
+            {
+                int tentativo = int.Parse(Console.ReadLine()!);
+                tentativi++;
+                if (tentativo == numero)
+                {
+                    Console.WriteLine($"Hai indovinato in {tentativi} tentativi");
+                    punteggio += 10 - tentativi;
+                    Console.WriteLine($"Il tuo punteggio è {punteggio}");
+                    break;
+                }
+                else
+                {
+                    if (tentativo > numero)
+                    {
+                        Console.WriteLine("Il numero è minore");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Il numero è maggiore");
+                    }
+                }
+                if (tentativi == 10)
+                {
+                    Console.WriteLine("Hai esaurito i tentativi");
+                    break;
+                }
+            }
+            Console.WriteLine("Vuoi continuare? (s/n)");
+            string risposta = Console.ReadLine()!;
+            if (risposta == "n")
+            {
+                break;
+            }
+        }
+    }
 }
--- Indovina-il-numero/Program.cs
+++ Indovina-il-numero/Program.cs
@@ -1,178 +1,222 @@
 ﻿class Program
 {
-    static void Main(string[] args)
-    {
-        // programma che genera un numero tra 1 e 100 e chiede di indivinare il numero
-        // se sbaglio esce, se indovino mi avvisa con un output. Ho a disposizione 
-        // 10 tentativi
+	static void Main(string[] args)
+	{
 
-        Random random = new();
-        int x = random.Next(1,101);
-        int input, tentativi = 10;
-        
-				// Console.WriteLine($"{x}");
-				
-        Console.Clear();
-        
-        Console.WriteLine("Prova ad indovinare il numero segreto");
-        input = int.Parse(Console.ReadLine()!);
+		int punteggio = 0;
+		bool end = false;
 
-		
+		// Console.WriteLine($"{x}");
 
-        while(tentativi != 0) // ripeto fino a quando hai l'ultimo tentativo poi finisce il gioco
-        {
-            if (input == x)
-            {
-              Console.WriteLine("Che fortuna!!!");
-              return;
-            }
+		while (!end) // ripeto fino a quando hai l'ultimo tentativo poi finisce il gioco
+		{
 
-						tentativi--; // se non hai azzeccato ti leva un tentativo
-            Console.WriteLine($"Mi dispiace, hai ancora {tentativi} tentativi");
+			Random random = new();
+			int x = random.Next(1, 101);
+			int input, tentativi = 10;
+			// Console.Clear();
+			Console.WriteLine("Prova ad indovinare il numero segreto");
+			input = int.Parse(Console.ReadLine()!);
 
-		    		switch (tentativi)
-            	{
-								case 9:
-            	  	{
-										bool maggiore50 = x > 50;
-            	  	  Console.WriteLine($"il numero e' maggiore di 50? {maggiore50}");
-										break;
-            	  	}
-								case 8:
-									{
-										bool pari = x % 2 == 0;
-										Console.WriteLine($"il numero e' pari? {pari}");
-										break;
+			bool maggiore5 = x > 5;
+			bool maggiore10 = x > 10;
+			bool maggiore15 = x > 15;
+			bool maggiore20 = x > 20;
+			bool maggiore25 = x > 25;
+			bool maggiore30 = x > 30;
+			bool maggiore35 = x > 35;
+			bool maggiore40 = x > 40;
+			bool maggiore45 = x > 45;
+			bool maggiore50 = x > 50;
+			bool maggiore55 = x > 55;
+			bool maggiore60 = x > 60;
+			bool maggiore65 = x > 65;
+			bool maggiore70 = x > 70;
+			bool maggiore75 = x > 75;
+			bool maggiore80 = x > 80;
+			bool maggiore85 = x > 85;
+			bool maggiore90 = x > 90;
+			bool maggiore95 = x > 95;
+
+			while (tentativi >= 0)
+			{
+
+				if (input == x)
+				{
+					Console.WriteLine("Che fortuna!!!");
+					int giri = 10 - tentativi;
+					punteggio += tentativi;
+					Console.WriteLine($"Hai indovinato in {giri} tentativi");
+					Console.WriteLine($"il tuo punteggio e' {punteggio}");
+
+					Console.Write("---   ");
+					Thread.Sleep(200);
+					Console.Write("---   ");
+					Thread.Sleep(200);
+					Console.WriteLine("---");
+					Thread.Sleep(200);
+					Console.WriteLine("Vuoi continuare a giocare? s/n");
+					string risposta = Console.ReadLine()!;
+					if (risposta == "n")
+					{
+						end = true;
+					}
+					break;
+				}
+
+				else
+				{
+					tentativi--; // se non hai azzeccato ti leva un tentativo
+					Console.WriteLine($"Mi dispiace, hai ancora {tentativi} tentativi");
+
+					switch (tentativi)
+					{
+						case 9:
+							{
+								Console.WriteLine($"il numero e' maggiore di 50? {maggiore50}");
+								break;
+							}
+						case 8:
+							{
+								bool pari = x % 2 == 0;
+								Console.WriteLine($"il numero e' pari? {pari}");
+								break;
+							}
+						case 7:
+							{
+								if (maggiore50)
+								{
+									Console.WriteLine($"il numero e' maggiore di 75? {maggiore75}");
+									break;
+								}
+								Console.WriteLine($"il numero e' maggiore di 25? {maggiore25}");
+								break;
+							}
+							case 6:
+							{
+								Console.WriteLine("Not even close Bro");
+								break;
+							}
+
+						case 5:
+							{
+								bool tra0e25 = x < 25;
+								bool tra25e50 = x > 25 && x < 50;
+								bool tra50e75 = x > 50 && x < 75;
+								bool tra75e100 = x > 75;
+
+								if (tra75e100)
+								{
+									Console.WriteLine($"il numero e' maggiore di 90? {maggiore90}");
+									break;
+								}
+								else if (tra50e75)
+								{
+									Console.WriteLine($"il numero e' maggiore di 60? {maggiore60}");
+									break;
+								}
+								else if (tra25e50)
+								{
+									Console.WriteLine($"il numero e' maggiore di 40? {maggiore40}");
+									break;
+								}
+								else
+								{
+									Console.WriteLine($"il numero e' maggiore di 10? {maggiore10}");
+									break;
+								}
+							}
+						case 4:
+							{
+								if (x > 10)
+								{
+									int somma = 0;
+									somma += x % 10; //aggiungo a somma l'ultima cifra di num;
+									int a = x / 10;
+									somma += a; //divido num per 10, essendo intero non ci sarà parte decimale
+									Console.WriteLine($"La somma delle cifre che compongono il numero e' {somma}");
+								}
+									/* alex way 
+
+									while (true)
+        					{
+            				int somma = 0;
+            				int numeroTemporaneo = numero;
+            				while (numeroTemporaneo > 0)
+            				{
+            				    somma += numeroTemporaneo % 10;
+            				    numeroTemporaneo /= 10;
+            				}
+
 									}
-								case 7:
-            	  	{
-										bool maggiore50 = x > 50;
-										if (maggiore50)
-										{
-											bool maggiore70 = x > 70;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 70? {maggiore70}");
-											break;
-										} 
-										bool maggiore30 = x > 30;
-            	  	  Console.WriteLine($"il numero e' maggiore di 30? {maggiore30}");
-										break;  
-            	  	}
-						
-								case 5:
-            	  	{
-										bool tra0e25 = x < 25;
-										bool tra25e50 = x > 25 && x < 50;
-										bool tra50e75 = x > 50 && x < 75;
-										bool tra75e100 = x > 75;
 
-										if (tra75e100)
-										{
-											bool maggiore90 = x > 90;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 90? {maggiore90}");
-											break;
-										}
-										else if (tra50e75)
-										{
-											bool maggiore60 = x > 60;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 60? {maggiore60}");
-											break;
-										}
-										else if (tra25e50)
-										{
-											bool maggiore40 = x > 40;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 40? {maggiore40}");
-											break;
-										}
-										else 
-										{
-											bool maggiore10 = x > 10;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 10? {maggiore10}");
-											break;
-            	  		}
-									}
-								case 4:
-									{
-										if (x > 10)
-										{
-											int somma = 0;
-											somma += x % 10; //aggiungo a somma l'ultima cifra di num;
-     									int a = x/10; 
-											somma += a; //divido num per 10, essendo intero non ci sarà parte decimale
-											Console.WriteLine($"La somma delle cifre che compongono il numero e' {somma}");
+									*/
 
-											// alex way 
-										}
-										break;
-									}
-								case 3:
-            	  	{
-										bool tra0e10 = x < 10;
-										bool tra10e25 = x > 10 & x < 25;
-										bool tra25e40= x > 25 & x < 40;
-										bool tra40e50 = x > 40 & x < 50;
-										bool tra50e60 = x > 50 & x < 60;
-										bool tra60e75 = x > 60 & x < 75;
-										bool tra75e90 = x > 75 & x < 90;
+									break;
+								}
+						case 3:
+							{
+								bool tra0e10 = x < 10;
+								bool tra10e25 = x > 10 & x < 25;
+								bool tra25e40 = x > 25 & x < 40;
+								bool tra40e50 = x > 40 & x < 50;
+								bool tra50e60 = x > 50 & x < 60;
+								bool tra60e75 = x > 60 & x < 75;
+								bool tra75e90 = x > 75 & x < 90;
 
-										if (tra0e10)
-										{
-											bool maggiore5 = x > 5;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 5? {maggiore5}");
-											break;
-										}
-										else if (tra10e25)
-										{
-											bool maggiore20 = x > 20;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 20? {maggiore20}");
-											break;
-										}
+								if (tra0e10)
+								{
+									Console.WriteLine($"il numero e' maggiore di 5? {maggiore5}");
+									break;
+								}
+								else if (tra10e25)
+								{
+									Console.WriteLine($"il numero e' maggiore di 20? {maggiore15}");
+									break;
+								}
 
-										else if (tra25e40)
-										{
-											bool maggiore30 = x > 30;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 30? {maggiore30}");
-											break;
-										}
-										else if (tra40e50)
-										{
-											bool maggiore45 = x > 45;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 45? {maggiore45}");
-											break;
-										}
-										else if (tra50e60)
-										{
-											bool maggiore55 = x > 55;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 55? {maggiore55}");
-											break;
-										}
-										else if (tra60e75)
-										{
-											bool maggiore70 = x > 70;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 70? {maggiore70}");
-											break;
-										}
-										else if (tra75e90)
-										{
-											bool maggiore80 = x > 80;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 80? {maggiore80}");
-											break;
-										}
-										else 
-										{
-											bool maggiore95 = x > 95;
-            	  	  	Console.WriteLine($"il numero e' maggiore di 95? {maggiore95}");
-											break;
-            	  		}
-									}
-										case 0: 
-										{
-        							Console.WriteLine($"HAI PERSO!\nIl numero era {x}"); // finito il ciclo while (e i tentativi) si arriva qua
-											return;
-										}
-							};
-            input = int.Parse(Console.ReadLine()!);
-        }
+								else if (tra25e40)
+								{
+									Console.WriteLine($"il numero e' maggiore di 30? {maggiore35}");
+									break;
+								}
+								else if (tra40e50)
+								{
+									Console.WriteLine($"il numero e' maggiore di 45? {maggiore45}");
+									break;
+								}
+								else if (tra50e60)
+								{
+									Console.WriteLine($"il numero e' maggiore di 55? {maggiore55}");
+									break;
+								}
+								else if (tra60e75)
+								{
+									Console.WriteLine($"il numero e' maggiore di 65? {maggiore65}");
+									break;
+								}
+								else if (tra75e90)
+								{
+									Console.WriteLine($"il numero e' maggiore di 85? {maggiore85}");
+									break;
+								}
+								else
+								{
+									Console.WriteLine($"il numero e' maggiore di 95? {maggiore95}");
+									break;
+								}
+							}
+						case 0:
+							{
+								Console.WriteLine($"HAI PERSO!\nIl numero era {x}"); // finito il ciclo while (e i tentativi) si arriva qua
+								return;
+							}
+					};
+					input = int.Parse(Console.ReadLine()!);
+				}
 
-        
-    }
+			}
+		}
+
+	}
 }
