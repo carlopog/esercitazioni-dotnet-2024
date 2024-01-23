022a066c080c7a0ca074b730ea22b8a19cb47142
carlopog <carlo.poggi98@gmail.com>
2024-01-23 11:36:01 +0100

    eccezione uno

--- Esercitazione_1/Program.cs
+++ Esercitazione_1/Program.cs
@@ -2,50 +2,13 @@
 {
     static void Main(string[] args)
     {
-        int toprand = 1001;
-        Random random = new Random();
-        int numero = random.Next(1, toprand);
-        Console.WriteLine("Indovina il numero sorteggiato");
-        int tentativi = 0;
-        
-        while (true)
+        Console.WriteLine("Inserisci un numero tra 1 e 10");
+        int numero = int.Parse(Console.ReadLine()!); // il ? serve per dire che potrebbe essere null e si puo' fare anche con il !
+        if (numero < 1 || numero > 10)
         {
-            // suggerisci la somma delle cifre del numero
-            int somma = 0;
-            int numeroTemporaneo = numero;
-            while (numeroTemporaneo > 0)
-            {
-                somma += numeroTemporaneo % 10;
-                numeroTemporaneo /= 10;
-            }
-
-            int tentativo = int.Parse(Console.ReadLine()!);
-            tentativi++;
-            if (tentativo == numero)
-            {
-                Console.WriteLine($"Hai indovinato in {tentativi} tentativi");
-                break;
-            }
-            else
-            {
-                if (tentativo > numero)
-                {
-                    Console.WriteLine("Il numero è minore");
-                }
-                else
-                {
-                    Console.WriteLine("Il numero è maggiore");
-                }
-                Console.WriteLine($"La somma delle cifre del numero è {somma}");
-            }
-            if (tentativi == 5)
-            {
-                Console.WriteLine("Hai esaurito i tentativi");
-                // stampa il numero da indovinare
-                Console.WriteLine($"Il numero da indovinare era {numero}");
-
-                break;
-            }
+          Console.WriteLine("il numero non e' valido");
+          return;
         }
+        Console.WriteLine($"il numero inserito e' il numero {numero}");
     }
 }
