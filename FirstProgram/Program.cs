fc6fb42ea177893c63aa7c66a80b095a6f7cc54e
carlopog <carlo.poggi98@gmail.com>
2024-01-25 11:32:24 +0100

    aggiungere appunti sul foreach

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -23,5 +23,30 @@
   }
 }
 
-//  Esercizio: un array di stringhe con 3 nomi, cicliamo con un foreach, se il nome e' Mario, aggiungere a una lista 
-//  aggiungeremo un ReafKey e un ReadLine
+/*  Esercizio: un array di stringhe con 3 nomi, cicliamo con un foreach, se il nome e' Mario, aggiungere a una lista 
+ aggiungeremo un ReadKey e un ReadLine
+
+per ciclare su un dictionary sempre con il foreach
+ad esempio
+			Dictionary<int, int> step = new()
+								{
+									{0, 10},
+									{10, 25},
+									{25, 40},
+									{40, 50},
+									{50, 60},
+									{60, 75},
+									{75, 90},
+									{90, 100}
+								};
+								foreach (KeyValuePair<int, int> entry in step)
+								{
+									int y = (entry.Key + entry.Value) / 2;
+									if (Func1(x, entry.Key, entry.Value))
+									{
+										Function2(x, y);
+										break;
+									}
+								}
+
+ */
