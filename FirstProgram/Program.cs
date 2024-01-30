d09fabc99905e7da0d83ad1b7a7462c775be05a1
carlopog <carlo.poggi98@gmail.com>
2024-01-30 10:12:26 +0100

    test split on csv file

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -2,11 +2,20 @@
 {
   static void Main(string[] args)
   {
-      string path = @"test.txt";
-      string[] lines = File.ReadAllLines(path);
-      lines[^2] = "Ciao cane";
-      lines[^2] += " Bau";
-      lines[^3] += " Bau";
-      File.WriteAllLines(path, lines);
+    string path = @"test.csv";
+    string[] lines = File.ReadAllLines(path);
+    string[][] nomi = new string[lines.Length][]; // array di array sarebbe una matrice righe e colonne
+    for (int i = 0; i < lines.Length; i++)
+    {
+      nomi[i] = lines[i].Split(',');
+    }
+    foreach (string[] nome in nomi)
+    {
+      foreach (string n in nome)
+      {
+        Console.Write(n + " "); // stampa la riga
+      }
+      Console.WriteLine();
+    }
   }
 }
