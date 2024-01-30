b24a25041297ca74e3256a75a3810d0b9256b7fb
carlopog <carlo.poggi98@gmail.com>
2024-01-30 13:06:56 +0100

    add total case

--- Dadi/Program.cs
+++ Dadi/Program.cs
@@ -15,12 +15,12 @@
   {
     Random random = new();
     int[] d = new int[3];
-    int somma = 0;
     int input;
     int input2;
     int quota = 1;
     int scommessa = 0;
     int bottino = 100;
+    bool guess = false;
 
     Console.ForegroundColor = ConsoleColor.Magenta;
     Console.WriteLine($"Ciao Bello il tuo budget di partenza è {bottino}");
@@ -28,6 +28,7 @@
 
     while (bottino > 0)
     {
+      int somma = 0;
       for (int i = 0; i < 3; i++)
       {
         int n = random.Next(1, 7);
@@ -75,8 +76,8 @@
         {
           try
           {
-            input = int.Parse(Console.ReadLine()!); 
-          
+            input = int.Parse(Console.ReadLine()!);
+
             if (input < 1 || input > 6)
             {
               throw new ArgumentOutOfRangeException();
@@ -150,7 +151,7 @@
         Console.WriteLine($"Hai selezionato Doppia");
         Console.WriteLine($"QUOTA: {quota}x la tua scommessa");
         Console.ResetColor();
-        Doppia:
+      Doppia:
         Console.WriteLine("inserisci il valore singolo che credi uscirà due volte");
         {
           try
@@ -182,7 +183,7 @@
       }
       else if (type == "3")
       {
-        Triple:
+      Triple:
         Console.WriteLine("Vuoi scommettere su una tripla generica o specifica? g / s");
         string gs = Console.ReadLine()!;
         if (gs == "g")
@@ -258,13 +259,13 @@
         quota = 5;
         Console.WriteLine($"Hai selezionato Combinazione, per vincere devi azzeccare 2 dei 3 numeri usciti");
         Console.WriteLine($"QUOTA: {quota}x la tua scommessa");
-           ValoreUno:
-             Console.WriteLine("inserisci il primo dei due valori singoli che credi usciranno");
+      ValoreUno:
+        Console.WriteLine("inserisci il primo dei due valori singoli che credi usciranno");
         {
           try
           {
-            input = int.Parse(Console.ReadLine()!); 
-          
+            input = int.Parse(Console.ReadLine()!);
+
             if (input < 1 || input > 6)
             {
               throw new ArgumentOutOfRangeException();
@@ -279,13 +280,13 @@
             goto ValoreUno;
           }
         }
-         ValoreDue:
+      ValoreDue:
         Console.WriteLine($"inserisci anche il secondo valore");
         {
           try
           {
-            input2 = int.Parse(Console.ReadLine()!); 
-          
+            input2 = int.Parse(Console.ReadLine()!);
+
             if (input2 < 1 || input2 > 6)
             {
               throw new ArgumentOutOfRangeException();
@@ -300,65 +301,238 @@
             goto ValoreDue;
           }
         }
-
-
         bool uguale = false;
         bool combo = false;
         Results(d);
         foreach (int dado in d)
         {
-            if (dado == input)
-            {
-                uguale = true;
-            }
+          if (dado == input)
+          {
+            uguale = true;
+          }
         }
         if (uguale)
         {
-            foreach (int dado in d)
+          foreach (int dado in d)
+          {
+            if (dado == input2)
             {
-                if (dado == input2)
-                {
-                    combo = true;
-                }
+              combo = true;
             }
-            if (combo)
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("Hai indovinato, è uscita la combinazione indicata da te!");
-                Console.ResetColor();
-                bottino += quota * scommessa;
-            }
-            else
-            {
-                Console.WriteLine($"Non hai indovinato, Hai perso {scommessa} euro");
-                bottino -= scommessa;
-            }
+          }
+          if (combo)
+          {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hai indovinato, è uscita la combinazione indicata da te!");
+            Console.ResetColor();
+            bottino += quota * scommessa;
+          }
+          else
+          {
+            Console.WriteLine($"Non hai indovinato, Hai perso {scommessa} euro");
+            bottino -= scommessa;
+          }
         }
         else
         {
-            Console.WriteLine($"Non hai indovinato, Hai perso {scommessa} euro");
-            bottino -= scommessa;
+          Console.WriteLine($"Non hai indovinato, Hai perso {scommessa} euro");
+          bottino -= scommessa;
         }
       }
       else if (type == "5")
       {
-        Console.WriteLine("inserisci il valore totale che credi uscirà (ovvero la somma dei 3 dadi)");
-      }
-      else
-      {
-        Console.WriteLine("input non valido");
-        goto Scrivi;
-      }
+      Totale:
+        Console.WriteLine("Vuoi scommettere su un totale specifico o un gruppo? s / g");
+        string sg = Console.ReadLine()!;
+        if (sg == "s")
+        {
+        TotaleS:
+          Console.WriteLine("inserisci il valore totale che credi uscirà (ovvero la somma dei 3 dadi)");
+          {
+            try
+            {
+              input = int.Parse(Console.ReadLine()!);
+              if (input < 3 || input > 18)
+              {
+                throw new ArgumentOutOfRangeException();
+              }
+            }
+            catch (Exception)
+            {
+              Console.BackgroundColor = ConsoleColor.Magenta;
+              Console.WriteLine("Non è un numero valido: dev'essere un intero tra 1 e 6  ");
+              Console.ResetColor();
+              Console.WriteLine("");
+              goto TotaleS;
+            }
+          }
+          switch (input)
+          {
+            case 4:
+            case 17:
+              {
+                quota = 50;
+                break;
+              }
+            case 5:
+            case 16:
+              {
+                quota = 25;
+                break;
+              }
+            case 6:
+            case 15:
+              {
+                quota = 20;
+                break;
+              }
+            case 7:
+            case 14:
+              {
+                quota = 10;
+                break;
+              }
+            case 8:
+            case 13:
+              {
+                quota = 8;
+                break;
+              }
+            default:
+              {
+                quota = 6;
+                break;
+              }
+          }
+          Results(d);
+          Console.WriteLine($"\nLa loro somma fa {somma}");
+          if (input == somma)
+          {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Quindi hai indovinato, è uscita la combinazione indicata da te!");
+            Console.ResetColor();
+            bottino += quota * scommessa;
+          }
+          else
+          {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Quindi non hai indovinato, Hai perso {scommessa} euro");
+            bottino -= scommessa;
+            Console.ResetColor();
+          }
+        }
+        else if (sg == "g")
+        {
+        TotaleG:
+          Console.WriteLine("In quale gruppo pensi che rientrerà la somma totale?");
+          Console.WriteLine("1. Big: la somma totale dei dadi è compresa tra 11 e 17");
+          Console.WriteLine("2. Small: la somma totale dei dadi è compresa tra 4 e 10");
+          Console.WriteLine("3. Pari: la somma totale dei dadi è un numero pari");
+          Console.WriteLine("4. Dispari: la somma totale dei dadi è un numero dispari");
+          Console.WriteLine("5. Estremi: la somma totale dei dadi è 3 o 18");
+          {
+            try
+            {
+              input = int.Parse(Console.ReadLine()!);
+              if (input < 1 || input > 5)
+              {
+                throw new ArgumentOutOfRangeException();
+              }
+            }
+            catch (Exception)
+            {
+              Console.BackgroundColor = ConsoleColor.Magenta;
+              Console.WriteLine("Non è un numero valido: dev'essere un intero tra 1 e 6  ");
+              Console.ResetColor();
+              Console.WriteLine("");
+              goto TotaleG;
+            }
+          }
+          switch (input)
+          {
+            case 1:
+              {
+                if (somma > 10 && somma < 18)
+                {
+                  guess = true;
+                }
+                break;
+              }
+            case 2:
+              {
+                if (somma > 3 && somma < 11)
+                {
+                  guess = true;
+                }
+                break;
+              }
+            case 3:
+              {
+                if (somma % 2 == 0)
+                {
+                  guess = true;
+                }
+                break;
+              }
+            case 4:
+              {
+                if (somma % 2 == 0)
+                {
+                  guess = false;
+                }
+                else
+                {
+                  guess = true;
+                }
+                break;
+              }
+            case 5:
+              {
+                if (somma == 3 || somma == 18)
+                {
+                  guess = true;
+                }
+                break;
+              }
+          }
+          Results(d);
+          Console.WriteLine($"\nIl totale è {somma}");
+          if (guess)
+          {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Quindi hai indovinato, è nel gruppo indicato da te!");
+            Console.ResetColor();
+            bottino += quota * scommessa;
+          }
+          else
+          {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Quindi non hai indovinato, Hai perso {scommessa} euro");
+            bottino -= scommessa;
+            Console.ResetColor();
+          }
+        }
+        else
+          {
+            Console.WriteLine("input non valido");
+            goto Totale;
+          }
+        }
+        else
+        {
+          Console.WriteLine("input non valido");
+          goto Scrivi;
+        }
 
-      Console.ForegroundColor = ConsoleColor.Magenta;
-      Console.WriteLine($"il tuo bottino è di {bottino} euro");
-      Console.ResetColor();
-      if (bottino == 0)
-      {
-        Console.BackgroundColor = ConsoleColor.Red;
-        Console.WriteLine($"Vattene via pezzente!");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine($"il tuo bottino è di {bottino} euro");
         Console.ResetColor();
+        if (bottino == 0)
+        {
+          Console.BackgroundColor = ConsoleColor.Red;
+          Console.WriteLine($"Vattene via pezzente!");
+          Console.ResetColor();
+        }
       }
     }
   }
-}
