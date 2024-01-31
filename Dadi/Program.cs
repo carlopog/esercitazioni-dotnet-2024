cf5351de36d655f6e1d9c65533006f6a6cb6ca0f
carlopog <carlo.poggi98@gmail.com>
2024-01-31 15:27:47 +0100

    add auth to dadi

--- Dadi/Program.cs
+++ Dadi/Program.cs
@@ -13,6 +13,102 @@
 
   static void Main(string[] args)
   {
+    string pathGiocatori = @"giocatori.csv";
+    string pathGiocatoreAttuale = @"giocatoreAttuale.csv";
+    if (!File.Exists(pathGiocatori))
+    {
+      File.Create(pathGiocatori).Close();
+    }
+    if (!File.Exists(pathGiocatoreAttuale))
+    {
+      File.Create(pathGiocatoreAttuale).Close();
+    }
+    int counter = 0;
+    string[] li = File.ReadAllLines(pathGiocatori);
+    string[] righe = new string[li.Length];
+    for (int i = 0; i < li.Length; i++)
+    {
+      if (!li[i].EndsWith(","))
+      {
+        righe[counter] = li[i];
+        counter++;
+      }
+    }
+    File.WriteAllLines(pathGiocatori, righe);
+    Console.WriteLine("Ciao, per giocare a Super Sic Bo devi essere maggiorenne, hai almeno 18 anni? (s/n)");
+    string siNo = Console.ReadLine()!; // legge il tipo di operazione scelta
+    if (siNo == "s")
+    {
+      Console.WriteLine("Come ti chiami?");
+      string nomeGiocatore = Console.ReadLine()!;
+      string[] ilGiocatoreAttuale = new string[1];
+      ilGiocatoreAttuale[0] = nomeGiocatore;
+      File.WriteAllLines(pathGiocatoreAttuale, ilGiocatoreAttuale);
+
+      if (File.ReadAllLines(pathGiocatori).Any(line => line.StartsWith(nomeGiocatore)))
+      {
+        for (int i = 0; i < righe.Length; i++)
+        {
+          string[] giocatoreNoto = righe[i].Split(',');
+          if (giocatoreNoto[0] == nomeGiocatore)
+          {
+            Console.WriteLine($"Ah bentornato {giocatoreNoto[0]}, mi ricordo di te, tu hai {giocatoreNoto[1]} anni");
+            int etaNota = int.Parse(giocatoreNoto[1]);
+            if (etaNota > 17)
+            {
+              Console.WriteLine("Sei maggiorenne, puoi giocare a Super Sic Bo");
+            }
+            else
+            {
+              Console.WriteLine("Non sei maggiorenne brutto bugiardo! Tornatene a casa!!");
+            }
+          }
+        }
+      }
+      else
+      {
+        File.AppendAllText(pathGiocatori, "\n" + nomeGiocatore + ",");
+        Age:
+        Console.WriteLine($"Quanti anni hai {nomeGiocatore}?");
+        try 
+        {
+          string eta = Console.ReadLine()!;
+          int age = int.Parse(eta);
+          File.AppendAllText(pathGiocatori, eta);
+           if (age >= 18)
+           {
+              Console.WriteLine($"Beh se hai {eta} anni allora puoi giocare");
+           }
+           else if (age < 13)
+           {
+              Console.WriteLine("Allora cosa dici di essere maggiorenne?? Vattene via bambinetto!");
+              return;
+           }
+           else if (age == 17)
+           {
+            Console.WriteLine($"17 anni non sono abbastanza. Torna l'anno prossimo ragazzo.");
+            return;    
+           }
+           else
+           {
+              Console.WriteLine("Allora non sei maggiorenne! Tornatene a casa pischello!");
+              return;
+           }
+
+        }
+        catch (Exception)
+        {
+          Console.WriteLine($"Non è un'età valida, inserisci un numero.");
+          goto Age;
+        }
+      }
+    }
+    else
+    {
+      Console.WriteLine($"allora non puoi giocare");
+      return;
+    }
+
     Random random = new();
     int[] d = new int[3];
     int input;
@@ -41,7 +137,8 @@
     File.AppendAllText(path2, "0" + "\n");    
 
     Console.ForegroundColor = ConsoleColor.Magenta;
-    Console.WriteLine($"Ciao Bello il tuo budget di partenza è {bottino}");
+    string[] giocatoreAttuale = File.ReadAllLines(pathGiocatoreAttuale);
+    Console.WriteLine($"Ciao {giocatoreAttuale[0]} il tuo budget di partenza è {bottino}");
     Console.ResetColor();
 
     while (bottino > 0)
