e651408aecb8b75f3a4185ac81b9a90e8749a93c
carlopog <carlo.poggi98@gmail.com>
2024-02-06 11:41:15 +0100

    function Ordine

--- JSON/Program.cs
+++ JSON/Program.cs
@@ -2,129 +2,74 @@
 
 class Program
 {
-
   static void Main(string[] args)
   {
-  string siNo;
-  string piatto;
-  string scelta;
-  int costoPiatto;
-  string pathPiatti = @"piatti.json";
+  string pathPiatti = @"piatti.json"; // menu con piatto e costo
   string piattiJson = File.ReadAllText(pathPiatti);
-  dynamic p = JsonConvert.DeserializeObject(piattiJson)!;
-  string[] piatti = new string[25];
-  string[] costoPiatti = new string[25];
-  string pathCassa = @"cassa.txt";
-  string pathOrdine = @"ordine.txt";
+  dynamic menu = JsonConvert.DeserializeObject(piattiJson)!;
 
-  Antipasti:
-  Console.Write("Buonasera signori, ");
-  Console.WriteLine($"cosa gradite come antipasti?");
+  string[] piatti = new string[25]; // array del menu 
+  string[] prezzi = new string[25]; // array dei costi
 
-  for (int i = 0; i < 5; i++)
-  {
-    piatti[i] = p[i].piatto;
-    costoPiatti[i] = p[i].prezzo;
-    Console.WriteLine($"{piatti[i]} a {costoPiatti[i]} euro");
-  }
+  Ordine("antipasti", piatti, prezzi, menu, 1);
+  Ordine("primi", piatti, prezzi, menu, 2);
+  Ordine("vini", piatti, prezzi, menu, 2);
+  Ordine("secondi", piatti, prezzi, menu, 2);
+  Ordine("vini", piatti, prezzi, menu, 2);
+  Ordine("dolci", piatti, prezzi, menu, 2);
 
-  scelta = Console.ReadLine()!;
-  for (int i = 0; i < 5; i++)
-  {
-    piatto = piatti[i];
-    costoPiatto = int.Parse(costoPiatti[i]);
-    if (scelta == piatto)
-    {
-      File.AppendAllText(pathOrdine, piatto + "\n");
-      File.AppendAllText(pathCassa, costoPiatto + "\n");
-    }
-  }
-  Console.WriteLine($"Volete ordinare altri antipasti? s/n");
-  siNo = Console.ReadLine()!;
-    if (siNo == "s")
-    {
-      goto Antipasti;
-    }
-  Primi:
-  Console.Write("Buonasera signori, ");
-  Console.WriteLine($"cosa gradite come primi?");
+  Console.WriteLine($"buon appetito");
 
-  for (int i = 5; i < 10; i++)
-  {
-    piatti[i] = p[i].piatto;
-    costoPiatti[i] = p[i].prezzo;
-    Console.WriteLine($"{piatti[i]} a {costoPiatti[i]} euro");
-  }
-
-  scelta = Console.ReadLine()!;
-  for (int i = 5; i < 10; i++)
-  {
-    piatto = piatti[i];
-    costoPiatto = int.Parse(costoPiatti[i]);
-    if (scelta == piatto)
-    {
-      File.AppendAllText(pathOrdine, piatto + "\n");
-      File.AppendAllText(pathCassa, costoPiatto + "\n");
-    }
-  }
-  Console.WriteLine($"Volete ordinare altri primi? s/n");
-  siNo = Console.ReadLine()!;
-    if (siNo == "s")
-    {
-      goto Primi;
-    }
-  // Vini:
-  //   Ordine(vini, 15, 20);
-  //   if (siNo == "s")
-  //   {
-  //     goto Vini;
-  //   }
-  // Secondi:
-  //   Ordine(secondi, 10, 15);
-  //   if (siNo == "s")
-  //   {
-  //     goto Secondi;
-  //   }
-  // Dolci:
-  //   Ordine(dolci, 20, 25);
-  //   if (siNo == "s")
-  //   {
-  //     goto Dolci;
-  //   }
-    Console.WriteLine($"buon appetito");
-
-  }
-
-
-// static void Ordine(string primo, int da, int a)
-// {
-//   Console.Write("Buonasera signori, ");
-//   Console.WriteLine($"cosa gradite come {primo}?");
-
-//   for (int i = da; i < a; i++)
-//   {
-//     piatti[i] = p[i].piatto;
-//     costoPiatti[i] = p[i].prezzo;
-//     Console.WriteLine($"{piatti[i]} a {costoPiatti[i]} euro");
-//   }
-
-//   scelta = Console.ReadLine()!;
-//   for (int i = da; i < a; i++)
-//   {
-//     piatto = piatti[i];
-//     costoPiatto = int.Parse(costoPiatti[i]);
-//     if (scelta == piatto)
-//     {
-//       File.AppendAllText(pathOrdine, piatto + "\n");
-//       File.AppendAllText(pathCassa, costoPiatto + "\n");
-//     }
-//   }
-//   Console.WriteLine($"Volete ordinare altri {primo}? s/n");
-//   siNo = Console.ReadLine()!;
-// }
-
-
-  //  string[] conti = File.ReadAllLines(pathCassa);
+  //  string[] conti = File.ReadAllLines(pathCassa);do
   //  string[] piatti = File.ReadAllLines(pathOrdine);
+
+// cassa: cosa hai comprato e quanto hai speso?
+
+  }
+
+
+static void Ordine(string tipi, string[] piatti, string[] costi, dynamic menu, int tavolo)
+{
+  Inizio:
+  Console.Write("Buonasera signori, ");
+  Console.WriteLine($"cosa gradite come {tipi}?");
+
+  string a; 
+
+  for (int i = 0; i < 25; i++)
+  {
+    piatti[i] = menu[i].piatto;
+    costi[i] = menu[i].prezzo;
+    a = menu[i].tipo;
+    if (tipi == a)
+    {
+      Console.WriteLine($"{piatti[i]} a {costi[i]} euro");
+    }
+  }
+
+  string pathOrdine = tavolo + "ordine.txt";
+  string pathCassa = tavolo + "cassa.txt";
+  string piatto; // piatto singolo
+  int costo; // costo del singolo
+
+  string scelta = Console.ReadLine()!;
+  for (int i = 0; i < 25; i++)
+  {
+    piatto = piatti[i];
+    costo = int.Parse(costi[i]);
+    if (scelta == piatto)
+    {
+      File.AppendAllText(pathOrdine, piatto + "\n");
+      File.AppendAllText(pathCassa, costo + "\n");
+    }
+  }
+  Console.WriteLine($"Volete ordinare altri {tipi}? s/n");
+  string siNo = Console.ReadLine()!;
+    if (siNo == "s")
+    {
+      goto Inizio;
+    }
+}
+
 
 }
