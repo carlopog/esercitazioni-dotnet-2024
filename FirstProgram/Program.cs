bdd419199e00d521b1f5fe367a7dd23ee3017621
carlopog <carlo.poggi98@gmail.com>
2024-02-13 13:01:03 +0100

    fix method

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,23 +1,69 @@
 ﻿using Newtonsoft.Json;
 
 class Program {
-    
-    static void CreateFiles(string[] nomi)
-    {
-    
-    foreach (string nome in nomi)
-    {
-      string path = @$"{nome}test.json";
-      File.ReadAllLines(path);    
-      
-    }
-    }
-    
 
   static void Main(string[] args)
   {
-    string[] nomi = ["antipasti", "b", "c"];
-    CreateFiles(nomi);
-
+    string[] giocatore = ["Mario", "25", "200", "500"];
+  
+    Exit(giocatore);
   }
-}
+
+  static void Exit(string[] giocatore)
+  {
+    string nome = giocatore[0];
+    int eta = int.Parse(giocatore[1]);
+    int prestito = int.Parse(giocatore[2]);
+    int bottino = int.Parse(giocatore[3]);
+    if (prestito > 0)
+    {
+      bottino -= prestito;
+    }
+    Console.WriteLine("Vuoi continuare a giocare o uscire? c/u");
+    string scelta = Console.ReadLine()!;
+    string path = @"giocatori.csv";
+    if (scelta == "u")
+    {
+      if (!File.Exists(path))
+      {
+        File.Create(path).Close();
+      }
+       if (File.ReadAllLines(path).Any(line => line.StartsWith(nome)))
+        {
+          string[] righe = File.ReadAllLines(path);
+          string[][] players = new string[righe.Length][];
+          for (int i = 0; i < righe.Length; i++)
+          {
+            players[i] = righe[i].Split(',');
+          }
+          for (int i = 0; i < players.Length; i++)
+          {
+            if (players[i][0] == nome) // se il nome e' gia' presente nel file
+            {
+              players[i][2] = prestito.ToString();
+              players[i][3] = bottino.ToString();
+            }
+              string path3 = @"giocatori2.csv";
+              if (!File.Exists(path3))
+              {
+                File.Create(path3).Close();
+              }
+
+              foreach (string[] player in players)
+              {
+                File.AppendAllText(path3, player[0] + "," + int.Parse(player[1]) + "," + int.Parse(player[2]) + "," + int.Parse(player[3])  + "\n");
+              }
+              string[] nuoviDati = File.ReadAllLines(path3);
+              File.Delete(path);
+              File.Create(path).Close();
+              File.AppendAllLines(path, nuoviDati);
+              File.Delete(path3);
+            }
+          }
+        else
+        {
+          File.AppendAllText(path, nome + "," + eta + "," + prestito + "," + bottino  + "\n");
+        }
+        }
+    }
+  }
