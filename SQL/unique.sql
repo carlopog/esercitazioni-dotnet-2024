3b4ab2f86b6a72555c472e281e2517efd2ceae7d
carlopog <carlo.poggi98@gmail.com>
2024-02-21 12:44:55 +0100

    UNIQUE name

---
+++ SQL/unique.sql
@@ -0,0 +1,4 @@
+CREATE TABLE prodotti (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT UNIQUE, prezzo REAL, quantita INTEGER);
+INSERT INTO prodotti  (nome, prezzo, quantita) VALUES ('p1', 1, 10);
+INSERT INTO prodotti  (nome, prezzo, quantita) VALUES ('p2', 2, 20);
+INSERT INTO prodotti  (nome, prezzo, quantita) VALUES ('p3', 3, 30);
