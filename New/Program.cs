3503da3e2bbc2928df2cab7deab3c6517b171d5d
carlopog <carlo.poggi98@gmail.com>
2024-02-26 12:07:57 +0100

    add visualizza prodotti in base a un numero

--- New/Program.cs
+++ New/Program.cs
@@ -133,20 +133,18 @@ class Program
   static void SelezionaVisualizzazione(string prodotti, string numero)
   {
     int counter = 0;
+  Valori:
     Console.WriteLine($"Hai scelto {prodotti}. Come vuoi visualizzare?");
-    Console.WriteLine($"1 - Elenco completo");
-    Console.WriteLine($"2 - In base a un nome");
-    Console.WriteLine($"3 - In base a un id");
-    Console.WriteLine($"4 - In base a un valore");
-    Console.WriteLine($"5 - Dal valore maggiore al minore");
-    Console.WriteLine($"6 - Dal valore minore al maggiore");
+    Console.WriteLine("1 - Elenco completo");
+    Console.WriteLine("2 - In base a un nome");
+    Console.WriteLine("3 - In base a un id");
+    Console.WriteLine("4 - In base a un valore");
     string scelta = Console.ReadLine()!;
     switch (scelta)
     {
       case "1":
         {
           Console.WriteLine("Hai scelto Elenco completo:");
-
           SQLiteConnection connection = new SQLiteConnection($"Data Source=database.db;Version=3;"); // crea la connessione di nuovo perché è stata chiusa alla fine del while in modo da poter visualizzare i dati aggiornati
           connection.Open();
           string sql = $"SELECT * FROM {prodotti}"; // crea il comando sql che seleziona tutti i dati dalla tabella prodotti
@@ -162,15 +160,15 @@ class Program
         }
       case "2":
         {
-          Console.WriteLine($"Hai scelto di visualizzare un elenco in base al nome,");
-          Console.WriteLine($"Inserisci nome del giocatore di cui vuoi visualizzare:");
+          Console.WriteLine("Hai scelto di visualizzare un elenco in base al nome,");
+          Console.WriteLine("Inserisci nome del giocatore di cui vuoi visualizzare:");
           string nome = Console.ReadLine()!;
           SQLiteConnection connection = new SQLiteConnection($"Data Source=database.db;Version=3;");
           connection.Open();
           string sql = $"SELECT * FROM {prodotti} WHERE nome = '{nome}'";
           SQLiteCommand command = new SQLiteCommand(sql, connection);
           SQLiteDataReader reader = command.ExecuteReader();
-          
+
           while (reader.Read())
           {
             Console.WriteLine($"id: {reader["id"]}, nome: {reader["nome"]}, {numero}: {reader[$"{numero}"]}");
@@ -181,18 +179,197 @@ class Program
           if (counter == 0)
           {
             Console.WriteLine("Non c'è nessun dato corrispondente a questo nome");
-            
           }
           break;
         }
-      // case "3":
-      //   {
-      //     Console.WriteLine($"Hai scelto di {verbo} una vincita,");
-      //     prodotti = "vincite";
-      //     prodotto = "della vincita";
-      //     numero = "risultato";
-      //     break;
-      //   }
+
+      case "3":
+        {
+          Console.WriteLine("Hai scelto di visualizzare un elenco in base all'id,");
+          Console.WriteLine("Inserisci l'id che vuoi visualizzare:");
+          string id = Console.ReadLine()!;
+          SQLiteConnection connection = new SQLiteConnection($"Data Source=database.db;Version=3;");
+          connection.Open();
+          string sql = $"SELECT * FROM {prodotti} WHERE id = '{id}'";
+          SQLiteCommand command = new SQLiteCommand(sql, connection);
+          SQLiteDataReader reader = command.ExecuteReader();
+
+          while (reader.Read())
+          {
+            Console.WriteLine($"id: {reader["id"]}, nome: {reader["nome"]}, {numero}: {reader[$"{numero}"]}");
+            counter++;
+            Thread.Sleep(300);
+          }
+          connection.Close();
+          if (counter == 0)
+          {
+            Console.WriteLine("Non c'è nessun dato corrispondente a questo id");
+          }
+          break;
+        }
+
+      case "4":
+        {
+          Console.WriteLine("Hai scelto di visualizzare un elenco in base ad un valore numerico,");
+          Console.WriteLine("Cosa vuoi visualizzare?");
+          Console.WriteLine("1 - Elenco completo dal maggiore al minore");
+          Console.WriteLine("2 - Elenco completo dal minore al maggiore");
+          Console.WriteLine("3 - Valori superiori a un determinato numero");
+          Console.WriteLine("4 - Valori inferiori a un determinato numero");
+          Console.WriteLine("5 - Solo valori uguali a un determinato numero");
+          string numScelta = Console.ReadLine()!;
+          switch (numScelta)
+          {
+            case "1":
+              {
+                SQLiteConnection connection = new SQLiteConnection($"Data Source=database.db;Version=3;");
+                connection.Open();
+                string sql = $"SELECT * FROM {prodotti} ORDER BY {numero} DESC";
+                SQLiteCommand command = new SQLiteCommand(sql, connection);
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                  Console.WriteLine($"id: {reader["id"]}, nome: {reader["nome"]}, {numero}: {reader[$"{numero}"]}");
+                  counter++;
+                  Thread.Sleep(300);
+                }
+                connection.Close();
+                if (counter == 0)
+                {
+                  Console.WriteLine("Non c'è nessun dato");
+                }
+                break;
+              }
+            case "2":
+              {
+                SQLiteConnection connection = new SQLiteConnection($"Data Source=database.db;Version=3;");
+                connection.Open();
+                string sql = $"SELECT * FROM {prodotti} ORDER BY {numero} ASC";
+                SQLiteCommand command = new SQLiteCommand(sql, connection);
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                  Console.WriteLine($"id: {reader["id"]}, nome: {reader["nome"]}, {numero}: {reader[$"{numero}"]}");
+                  counter++;
+                  Thread.Sleep(300);
+                }
+                connection.Close();
+                if (counter == 0)
+                {
+                  Console.WriteLine("Non c'è nessun dato");
+                }
+                break;
+              }
+            case "3":
+              {
+              Superiore:
+                Console.WriteLine("Inserisci il valore del numero da paragonare");
+                try
+                {
+                  int paragone = int.Parse(Console.ReadLine()!);
+                  SQLiteConnection connection = new SQLiteConnection($"Data Source=database.db;Version=3;");
+                  connection.Open();
+                  string sql = $"SELECT * FROM {prodotti} DESC WHERE {numero} > {paragone}";
+                  SQLiteCommand command = new SQLiteCommand(sql, connection);
+                  SQLiteDataReader reader = command.ExecuteReader();
+
+                  while (reader.Read())
+                  {
+                    Console.WriteLine($"id: {reader["id"]}, nome: {reader["nome"]}, {numero}: {reader[$"{numero}"]}");
+                    counter++;
+                    Thread.Sleep(300);
+                  }
+                  connection.Close();
+                  if (counter == 0)
+                  {
+                    Console.WriteLine($"Non c'è nessun dato con un valore maggiore di {paragone.ToString()}");
+                  }
+                  break;
+                }
+                catch (Exception)
+                {
+                  Console.WriteLine("devi inserire un numero");
+                  goto Superiore;
+                }
+
+              }
+            case "4":
+              {
+              Inferiore:
+                Console.WriteLine($"Inserisci il valore del numero da paragonare");
+                try
+                {
+                  int paragone = int.Parse(Console.ReadLine()!);
+                  SQLiteConnection connection = new SQLiteConnection($"Data Source=database.db;Version=3;");
+                  connection.Open();
+                  string sql = $"SELECT * FROM {prodotti} DESC WHERE {numero} < {paragone}";
+                  SQLiteCommand command = new SQLiteCommand(sql, connection);
+                  SQLiteDataReader reader = command.ExecuteReader();
+
+                  while (reader.Read())
+                  {
+                    Console.WriteLine($"id: {reader["id"]}, nome: {reader["nome"]}, {numero}: {reader[$"{numero}"]}");
+                    counter++;
+                    Thread.Sleep(300);
+                  }
+                  connection.Close();
+                  if (counter == 0)
+                  {
+                    Console.WriteLine($"Non c'è nessun dato con un valore minore di {paragone}");
+                  }
+                  break;
+                }
+                catch (Exception)
+                {
+                  Console.WriteLine("devi inserire un numero");
+                  goto Inferiore;
+                }
+
+              }
+            case "5":
+              {
+              Inizio:
+                Console.WriteLine($"Inserisci il valore del numero da paragonare");
+                try
+                {
+                  int paragone = int.Parse(Console.ReadLine()!);
+                  SQLiteConnection connection = new SQLiteConnection($"Data Source=database.db;Version=3;");
+                  connection.Open();
+                  string sql = $"SELECT * FROM {prodotti} DESC WHERE {numero} = {paragone}";
+                  SQLiteCommand command = new SQLiteCommand(sql, connection);
+                  SQLiteDataReader reader = command.ExecuteReader();
+
+                  while (reader.Read())
+                  {
+                    Console.WriteLine($"id: {reader["id"]}, nome: {reader["nome"]}, {numero}: {reader[$"{numero}"]}");
+                    counter++;
+                    Thread.Sleep(300);
+                  }
+                  connection.Close();
+                  if (counter == 0)
+                  {
+                    Console.WriteLine($"Non c'è nessun dato con un valore uguale a {paragone}");
+                  }
+                  break;
+                }
+                catch (Exception)
+                {
+                  Console.WriteLine("devi inserire un numero");
+                  goto Inizio;
+                }
+
+              }
+            default:
+              {
+                Console.WriteLine($"devi scegliere un numero tra 1 e 5");
+                goto Valori;
+              }
+          }
+          break;
+        }
+
       // case "4":
       //   {
       //     Console.WriteLine($"Hai scelto di {verbo} un bottino,");
@@ -234,7 +411,7 @@ class Program
     Console.WriteLine(prodotti);
     Console.WriteLine(prodotto);
     Console.WriteLine(numero);
-    
+
 
 
     SelezionaVisualizzazione(prodotti, numero);
