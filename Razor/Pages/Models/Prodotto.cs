ad4d4864843201e85612952dc188969efda7dfd4
carlopog <carlo.poggi98@gmail.com>
2024-03-18 10:37:33 +0100

    add id & img

--- Razor/Pages/Models/Prodotto.cs
+++ Razor/Pages/Models/Prodotto.cs
@@ -1,7 +1,9 @@
 public class Prodotto
 {
+  public int Id { get; set; }
   public string? Nome { get; set; }
   public decimal Prezzo { get; set; }
-  public string? Dettaglio {get; set; }
+  public string? Dettaglio { get; set; }
+  public string? Immagine { get; set; }
 }
 
