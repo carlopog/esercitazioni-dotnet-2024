c230364152f2b6e3265adabc3774895342fc2609
carlopog <carlo.poggi98@gmail.com>
2024-03-18 12:17:20 +0100

    add card prodotto dettaglio da json

--- Razor/Pages/Prodotti.cshtml.cs
+++ Razor/Pages/Prodotti.cshtml.cs
@@ -15,22 +15,9 @@ public class ProdottiModel : PageModel
   public int numeroPagine { get; set; }
   public void OnGet(decimal? minPrezzo, decimal? maxPrezzo, int? pageIndex)
   {
-           var json = System.IO.File.ReadAllText("wwwroot/json/prodotti.json"); 
-        Prodotti = JsonConvert.DeserializeObject < List < Prodotto >>(json); 
-    // Prodotti = new List<Prodotto>
-    //   {
-    //     new Prodotto { Nome = "Prodotto 1", Prezzo = 100, Dettaglio = "Dettaglio 1"},
-    //     new Prodotto { Nome = "Prodotto 2", Prezzo = 200, Dettaglio = "Dettaglio 2"},
-    //     new Prodotto { Nome = "Prodotto 3", Prezzo = 300, Dettaglio = "Dettaglio 3"},
-    //     new Prodotto { Nome = "Prodotto 4", Prezzo = 400, Dettaglio = "Dettaglio 4"},
-    //     new Prodotto { Nome = "Prodotto 5", Prezzo = 500, Dettaglio = "Dettaglio 5"},
-    //     new Prodotto { Nome = "Prodotto 6", Prezzo = 600, Dettaglio = "Dettaglio 6"},
-    //     new Prodotto { Nome = "Prodotto 7", Prezzo = 700, Dettaglio = "Dettaglio 7"},
-    //     new Prodotto { Nome = "Prodotto 8", Prezzo = 800, Dettaglio = "Dettaglio 8"},
-    //     new Prodotto { Nome = "Prodotto 9", Prezzo = 900, Dettaglio = "Dettaglio 9"},
-    //     new Prodotto { Nome = "Prodotto 10", Prezzo = 1000, Dettaglio = "Dettaglio 10"},
-    //     new Prodotto { Nome = "Prodotto 11", Prezzo = 1100, Dettaglio = "Dettaglio 11"}
-    //   };
+    var json = System.IO.File.ReadAllText("wwwroot/json/prodotti.json"); 
+    Prodotti = JsonConvert.DeserializeObject < List < Prodotto >>(json); 
+  
       if (minPrezzo.HasValue)
       {
         Prodotti = Prodotti.Where(p => p.Prezzo >= minPrezzo);
--- Razor/Pages/ProdottoDettaglio.cshtml.cs
+++ Razor/Pages/ProdottoDettaglio.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json; 
+
 
 namespace Razor.Pages;
+
+
 public class ProdottoDettaglioModel : PageModel
 {
   public required Prodotto Prodotto {get; set;}
-  public void OnGet(string nome, string dettaglio, decimal prezzo)
+  public void OnGet(int id)
   {
-    Prodotto = new Prodotto { Nome = nome, Prezzo = prezzo, Dettaglio = dettaglio };
+    var numero = id - 1;
+    var json = System.IO.File.ReadAllText("wwwroot/json/prodotti.json"); 
+    List<Prodotto> Prodotti = JsonConvert.DeserializeObject<List<Prodotto>>(json);
+    Prodotto = Prodotti[numero];
   }
 }
