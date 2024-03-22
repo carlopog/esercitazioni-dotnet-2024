19546276d64de51e152dab262c24e4dced7c7bd3
carlopog <carlo.poggi98@gmail.com>
2024-03-22 16:22:33 +0100

    inizio partita doppia

---
+++ QUIZ/Pages/PartitaDoppia.cshtml.cs
@@ -0,0 +1,7 @@
+// creare la logica di una partita con domande
+// che hanno 3 opzioni
+// tutte e 3 selezionabili
+// due su tre sono giuste
+// o volendo una su tre (mettendo risposta1 e risposta2 uguali)
+// se hai crocettato la risposta sbagliata un punto in meno
+// se hai crocettato quella giusta 1 in piu'
