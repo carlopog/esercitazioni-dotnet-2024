22378af9c7c778f0dc8834f549c36e5b4a3b70ba
carlopog <carlo.poggi98@gmail.com>
2024-03-22 16:18:50 +0100

    add domande doppie

---
+++ QUIZ/Models/DomandaDoppia.cs
@@ -0,0 +1,10 @@
+public class DomandaDoppia
+{
+  public int id { get; set; }
+  public string domanda { get; set; }
+  public string opzioneA { get; set; }
+  public string opzioneB { get; set; }
+  public string opzioneC { get; set; }
+  public string risposta1 { get; set; } 
+  public string risposta2 { get; set; } 
+}
